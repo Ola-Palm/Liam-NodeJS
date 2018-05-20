5933bcbbe96b3b757003c33e4ab16bc59572bbee
Ola Palm <ola.palm@sr.se>
2018-05-20 13:15:36 +0200

    start idle and dock state possible to set from web

--- liam.js
+++ liam.js
@@ -1,204 +1,240 @@
 /*jshint esversion: 6 */
 (() => {
-        'use strict';
-        // content of index.js
+    'use strict';
+    // content of index.js
+    let Setcommands = {
+        'state': 202,
+    };
+    let topic = -1;
+    let mqtt = require('mqtt');
+    const server_port = 8000;
+    let voltAvg = [];
+    let arrayLen = 100;
+    // Web
+    var express = require('express');
+    var app = express();
+    var appApi = express();
+    const http = require('http');
+    let mower = {
+        'looptime': 0,
+        'avgvolt': 0,
+        'v_min': 10000,
+        'v_max': 0,
+        'state': 0,
+        'avg_len': 0,
+        'charge_starttime' : -1
+    }
 
-        let mqtt = require('mqtt');
-        const server_port = 8000;
-        let voltAvg=[];
-        let arrayLen = 100;
-        // Web
-        var express = require('express');
-        var app = express();
-        var appApi = express();
-        const http = require('http');
-        let mower ={
-            'looptime':0,
-            'avgvolt':0,
-            'v_min' : 10000,
-            'v_max' : 0,
-            'state' : 0,
-            'avg_len':0
-        }
+    // Server
+    var server = http.createServer(app);
 
-        // Server
-        var server = http.createServer(app);
+    // Socket
+    var io = require('socket.io')(server);
 
-        // Socket
-        var io = require('socket.io')(server);
+    //#region  MQTT
 
-        //#region  MQTT
+    let mqttToken = "abc123",
+        mqttclient;
+    var mqttopts = {
+        port: 1883,
+        host: "palm-home", // host does NOT include port
+        // hostname?: string
+        // path?: string
+        protocol: 'mqtt',
+        /**
+         *  10 seconds, set to 0 to disable
+         */
+        keepalive: 60 * 5,
+        /**
+         * 'mqttjs_' + Math.random().toString(16).substr(2, 8)
+         */
+        clientId: "Liam_" + Date.now(),
+        /**
+         * 'MQTT'
+         */
+        //  protocolId:"mqtt",
+        // // /**
+        // //  * 4
+        // //  */
+        // protocolVersion: "4",
+        /**
+        //  * true, set to false to receive QoS 1 and 2 messages while offline
+        //  */
+        clean: true,
+        /**
+         * 1000 milliseconds, interval between two reconnections
+         */
+        reconnectPeriod: 10 * 1000,
+        /**
+         * 30 * 1000 milliseconds, time to wait before a CONNACK is received
+         */
+        connectTimeout: 30 * 10000,
+        /**
+         * the username required by your broker, if any
+         */
+        username: "liam_" + Date.now(),
+        /**
+         * the password required by your broker, if any
+         */
+        // password?: string
+        /**
+         * a Store for the incoming packets
+        //  */
+        // incomingStore?: Store
+        // /**
+        //  * a Store for the outgoing packets
+        //  */
+        // outgoingStore?: Store
+        // queueQoSZero?: boolean
+        // reschedulePings?: boolean
+        // servers?: Array<{
+        //   host: string
+        //   port: number
+        // }>
+        // /**
+        //  * true, set to false to disable re-subscribe functionality
+        //  */
+        // resubscribe?: boolean
+        // /**
+        //  * a message that will sent by the broker automatically when the client disconnect badly.
+        //  */
+        // will?: {
+        //   /**
+        //    * the topic to publish
+        //    */
+        //   topic: string
+        //   /**
+        //    * the message to publish
+        //    */
+        //   payload: string
+        //   /**
+        //    * the QoS
+        //    */
+        //   qos: QoS
+        //   /**
+        //    * the retain flag
+        //    */
+        //   retain: boolean
+    };
 
-        let mqttToken = "abc123",
-            mqttclient;
-        var mqttopts = {
-            port: 1883,
-            host: "palm-home", // host does NOT include port
-            // hostname?: string
-            // path?: string
-            protocol: 'mqtt',
-            /**
-             *  10 seconds, set to 0 to disable
-             */
-            keepalive: 60 * 5,
-            /**
-             * 'mqttjs_' + Math.random().toString(16).substr(2, 8)
-             */
-            clientId: "Liam_"+Date.now(),
-            /**
-             * 'MQTT'
-             */
-            //  protocolId:"mqtt",
-            // // /**
-            // //  * 4
-            // //  */
-            // protocolVersion: "4",
-            /**
-            //  * true, set to false to receive QoS 1 and 2 messages while offline
-            //  */
-            clean: true,
-            /**
-             * 1000 milliseconds, interval between two reconnections
-             */
-            reconnectPeriod: 10 * 1000,
-            /**
-             * 30 * 1000 milliseconds, time to wait before a CONNACK is received
-             */
-            connectTimeout: 30 * 10000,
-            /**
-             * the username required by your broker, if any
-             */
-            username: "liam_"+Date.now(),
-            /**
-             * the password required by your broker, if any
-             */
-            // password?: string
-            /**
-             * a Store for the incoming packets
-            //  */
-            // incomingStore?: Store
-            // /**
-            //  * a Store for the outgoing packets
-            //  */
-            // outgoingStore?: Store
-            // queueQoSZero?: boolean
-            // reschedulePings?: boolean
-            // servers?: Array<{
-            //   host: string
-            //   port: number
-            // }>
-            // /**
-            //  * true, set to false to disable re-subscribe functionality
-            //  */
-            // resubscribe?: boolean
-            // /**
-            //  * a message that will sent by the broker automatically when the client disconnect badly.
-            //  */
-            // will?: {
-            //   /**
-            //    * the topic to publish
-            //    */
-            //   topic: string
-            //   /**
-            //    * the message to publish
-            //    */
-            //   payload: string
-            //   /**
-            //    * the QoS
-            //    */
-            //   qos: QoS
-            //   /**
-            //    * the retain flag
-            //    */
-            //   retain: boolean
-        };
+    mqttclient = mqtt.connect(mqttopts);
 
-        mqttclient = mqtt.connect(mqttopts);
+    mqttclient.on('error', (error) => {
+        console.log("# MQTT Baresip ERROR");
+    });
 
-        mqttclient.on('error', (error) =>
-        {
-            console.log("# MQTT Baresip ERROR");
-        });
+    mqttclient.on('offline', (error) => {
+        console.log("# MQTT Baresip host offline");
+    });
 
-        mqttclient.on('offline', (error) =>
-        {
-            console.log("# MQTT Baresip host offline");
-        });
+    mqttclient.on('close', (error) => {
+        console.log("# MQTT Baresip connection closed");
+    });
 
-        mqttclient.on('close', (error) =>
-        {
-            console.log("# MQTT Baresip connection closed");
-        });
+    mqttclient.on('connect', () => {
+        console.log("# MQTT is connected");
 
-        mqttclient.on('connect', () =>
-        {
-            console.log("# MQTT is connected");
+        mqttclient.subscribe('#');
+    });
 
-            mqttclient.subscribe('#');
-        });
+    mqttclient.on('message', (topic, message) => {
+        try {
+            switch (topic) {
+                case "/liam1/event":
 
-        mqttclient.on('message', (topic, message) =>
-        {
-            console.dir(message.toString('utf8'));
-            try {
-                switch (topic) {
-
-                    case "/liam1/event":
-                        let JMessage = JSON.parse(message);
-                        if(voltAvg.length >= arrayLen )
+                    let JMessage = JSON.parse(message);
+                    if(JMessage.State == 3)
+                    {
+                        /// Chargeing
+                        if(mower.state != JMessage.State)
                         {
-                            voltAvg.shift();
-                        }
-
-                        voltAvg.push(JMessage.SOC)
-                        let n = 0;
-                        let v = 0;
-                        let current=0;
-                        mower.V_min=10000;
-                        mower.V_max=0;
-                        for (let index = 0; index < voltAvg.length; index++) {
-                            current = voltAvg[index];
-                            v+= current
-                            n++;
-                            if(mower.v_min>current)
-                                mower.v_min = current;
-                            if(mower.v_max<current)
-                                mower.v_max = current;
-                        }
-                        let temp = JSON.stringify(mower);
-                        mower.avgvolt = (Math.floor(v / n) / 100);
-                        mower.looptime = JMessage.Looptime;
+                            var d = new Date();
+                            voltAvg.length=0; // set array to zero 
+                        mower.charge_starttime = d.toLocaleTimeString();
+                    }
                         mower.state = JMessage.State;
-                        mower["avg_len"] =  n;
-
-                        if (temp != JSON.stringify(mower))
-                            io.emit('battery_avg', mower);
-                        break;
-                    default:
-                        break;
+                        
+                        
 
                     }
-            } catch (error) {
-                console.log("ERROR "+ error);
+                    if (voltAvg.length >= arrayLen) {
+                        voltAvg.shift();
+                    }
+
+                    voltAvg.push(JMessage.SOC)
+                    let n = 0;
+                    let v = 0;
+                    let current = 0;
+                    mower.v_min = 10000;
+                    mower.v_max = 0;
+                    for (let index = 0; index < voltAvg.length; index++) {
+                        current = voltAvg[index];
+                        v += current
+                        n++;
+                        if (mower.v_min > current)
+                            mower.v_min = current;
+                        if (mower.v_max < current)
+                            mower.v_max = current;
+                    }
+                    let temp = JSON.stringify(mower);
+                    mower.avgvolt = (Math.floor(v / n) / 100);
+                    mower.looptime = JMessage.Looptime;
+                    mower.state = JMessage.State;
+                    mower["avg_len"] = n;
+
+                    if (temp != JSON.stringify(mower))
+                        io.emit('battery_avg', mower);
+                    break;
+                default:
+                    break;
+
             }
-        });
+        } catch (error) {
+            console.log("ERROR " + error);
+        }
+    });
 
-        //#endregion
+    //#endregion
 
 
-          //#region WEBSOCKET CLIENT --> SERVER
-    io.on('connection', (client) =>
-    {
+    //#region WEBSOCKET CLIENT --> SERVER
+    io.on('connection', (client) => {
         console.log('_ Websocket client connected');
-        client.on('join', (data) =>
-        {
-            console.log("Client connected");
-            client.emit('battery_avg',mower);
+        client.emit('mower__Init', mower);
+        client.on('join', (data) => {
+            client.emit('battery_avg', mower);
 
         }); // Join
 
-        client.on('disconnect', () =>
-        {
+        client.on('disconnect', () => {});
+        client.on('mower__action', (mower) => {
+            let mowerJson = {};
+            let cmd = -1;
+            try {
+                switch (mower.set_get) {
+                    case 'set':
+                        console.log('set value');
+                        topic = '/liam1/cmd/set';
+                        cmd = Setcommands[mower.command];
+                        if (cmd === -1)
+                            console.log("Detta gick inte bra. ");
+                        mowerJson[0] = cmd;
+                        mowerJson[1] = mower.value;
+                        mowerJson[99] = "123";
+                        console.dir(mower);
+                        break;
+                    case 'get':
+                        console.log(' get value');
+                        break;
+                    default:
+                        console.log("Defalut value");
+                        break;
+                }
+            } catch (error) {
+                console.log("Could nor parse json");
+                console.dir(mower);
+            }
+            publishCommnad(mowerJson);
         });
     });
 
@@ -207,32 +243,29 @@
 
 
 
-        app.use(express.static(__dirname + '/public'));
-        app.use(express.static(__dirname + '/node_modules'));
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/node_modules'));
 
 
-        app.get('/*', function (req, res, next)
-    {
+
+    app.get('/Liam', function (req, res, next) {
         console.log("=========================");
         res.sendFile(__dirname + '/public/html/index.html');
     });
-
-        app.get('/liam', function (req, res, next)
-    {
-        console.log("=========================");
-        res.sendFile(__dirname + '/public/html/index.html');
+    //#region Server
+    server.listen(server_port, function () {
+        console.log("> Server listening on : " + server_port);
     });
-   //#region Server
-   server.listen(server_port, function ()
-   {
-       console.log("> Server listening on : " +server_port);
-   });
 
-   server.on('error', function (e)
-   {
-       console.log("HTTPPORT is occupied, start as sudo or change port");
-       console.log(e);
-   });
+    server.on('error', function (e) {
+        console.log("HTTPPORT is occupied, start as sudo or change port");
+        console.log(e);
+    });
 
-}
-)();
+    function publishCommnad(mowerJson) {
+        console.log("detta är vad som kommer skickas mot klipparen.");
+        console.dir(mowerJson);
+        if (topic != -1)
+            mqttclient.publish(topic, JSON.stringify(mowerJson) + '\r\n');
+    }
+})();
--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,62 @@
 (() => {
     "use strict";
     var socket = io.connect();
-
+    let localMower={
+        state : -1
+    };
     window.onload = function () {
         this.console.log("laddat");
 
-        const button = document.getElementById('mower_State_action_button');
-        button.addEventListener('click', function(e) {
-          // push data to server. server komunicate with liam.
+        /// start mowing button
+        var mowingButton = document.getElementById('mower_State_action_button_start');
+        mowingButton.value = "Start mowing";
+
+        mowingButton.addEventListener('click', function (e) {
+            // push data to server. server komunicate with liam.
+            let n = localMower.state = 3 ? 1 : 0;
+            let mower = {
+                set_get: 'set',
+                command: 'state',
+                value: n
+            };
+            socket.emit("mower__action", mower);
         });
+          // idle button
+           var idleButton = document.getElementById('mower_State_action_button_stop');
+        idleButton.value = "Set IDLE";
+
+        idleButton.addEventListener('click', function(e) {
+          // push data to server. server komunicate with liam.
+          let mower = {
+            set_get: 'set',
+            command: 'state',
+            value : 4
+          };
+          socket.emit("mower__action",mower);
+        });
+           var dockingButton = document.getElementById('mower_State_action_button_dock');
+        dockingButton.value = "Set DOCK";
+
+        dockingButton.addEventListener('click', function(e) {
+          // push data to server. server komunicate with liam.
+          let mower = {
+            set_get: 'set',
+            command: 'state',
+            value : 2
+          };
+          socket.emit("mower__action",mower);
+        });
+
     }; // End window.onload
 
     socket.on("connect", function (data) {
-        console.log("What data comes out? :" + data);
+    });
+    socket.on("mower__Init", function (data) {
+        localMower.state = data.state;
     });
     socket.on("battery_avg", function (data) {
         console.dir(data);
+        localMower.state = data.state;
         var node = document.getElementById("battery_soc_current");
         node.innerHTML = "Avg Volt:"+ data.avgvolt + " out of:"+data.avg_len + " readings";
         node = document.getElementById("control_looptime");
@@ -24,8 +65,12 @@
         node.innerHTML = "State is " + data.state;
         node = document.getElementById("battery_soc_avg");
         node.innerHTML = "Volt Diff :" +(data.v_max - data.v_min)/100 + " / Min: " +data.v_min/100 +"V  / Max :"+ data.v_max/100 +"V";
-        node = document.getElementById("mower_State_action_button");
 
+        node = document.getElementById("control_runtime");
+        if(localMower.state ===3)
+        {
+            node.innerHTML = "Charging Start time :" + data.charge_starttime;
+        }
     });
 
 })()
